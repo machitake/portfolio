bf36630e5a6e7d9cd589037c21242c3fe5f50ca0
machitake <glory.tm5@gmail.com>
2021-07-08 23:55:39 +0900

    portfolio
    slick
    画像の見せ方

--- js/script.js
+++ js/script.js
@@ -15,30 +15,14 @@
     // autoplay:true,
     // autoplaySpeed:5000,
     dots:true,
-    slidesToShow:3, //表示するスライドの数
+    slidesToShow:2, //表示するスライドの数
     //slidesToScroll:2, //１度にスクロールする数
-    prevArrow: '<img src="./img/layer1.png" class="slide-arrow prev-arrow">',
-    nextArrow: '<img src="./img/layer2.png" class="slide-arrow next-arrow">',
 
     responsive:[
-      {
-          breakpoint: 1024,
-          settings:{
-              slidesToShow:3,
-          }
-      },
-      {
-          breakpoint: 768,
-          settings:{
-              slidesToShow:2,
-          }
-      },
       {
           breakpoint: 480,
           settings:{
               slidesToShow:1,
-              centerMode: true,
-              centerPadding: '10%'
           }
       },
   ]
